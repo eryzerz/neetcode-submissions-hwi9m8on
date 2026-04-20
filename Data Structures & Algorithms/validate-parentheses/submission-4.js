21879b4efafa1b80dc8be12e701de741ed5f16f9
Rizqullah T <eryzero@gmail.com>
2026-04-21 04:16:50 +0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        for (let i = 0; i < s.length; ++i) {
+            if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+                stack.push(s[i]);
+            } else {
+                if (s[i] === ")" && stack[stack.length - 1] === "(") {
+                    stack.pop();
+                } else if (s[i] === "}" && stack[stack.length - 1] === "{") {
+                    stack.pop();
+                } else if (s[i] === "]" && stack[stack.length - 1] === "[") {
+                    stack.pop();
+                } else {
+                    stack.push(0);
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
