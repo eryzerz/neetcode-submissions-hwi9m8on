8218fe9f462e75701bffe523061a43d8ddda42fc
Rizqullah T <eryzero@gmail.com>
2026-04-21 02:25:31 +0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-6

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-6.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        if (arr.length === 1) {
+            return [-1];
+        };
+
+        let max = 0;
+        let j = 0;
+
+        for (let k = j + 1; k < arr.length; ++k) {
+            if (arr[k] > arr[j]) {
+                max = arr[k];
+                arr[j] = max;
+                arr[k] = -1;
+                for (let i = 0; i < j; ++i) {
+                    if (i >= 0 && arr[j] > arr[i]) {
+                        arr[i] = max;
+                    }
+                }
+            } else if (arr[k] === arr[j]) {
+                arr[k] = -1;
+                max = arr[j];
+            } else {
+                max = arr[j];
+                arr[j] = arr[k];
+                arr[k] = -1;
+            }
+
+            j += 1;
+        }
+        return arr;
+    }
+}
