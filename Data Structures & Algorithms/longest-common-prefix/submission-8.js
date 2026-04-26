5e2783c1032731153fda61338c621a0a98a890c7
Rizqullah T <eryzero@gmail.com>
2026-04-27 00:46:15 +0700

    Add: longest-common-prefix - submission-8

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-8.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+
+        let result = "";
+
+        for (let i = 0; i < strs[0].length; ++i) {
+            let count = 0;
+            for (let j = 0; j < strs.length; ++j) {
+                let row = j === strs.length - 1 ? 0 : j + 1;
+                if (strs[j][i] === strs[row][i]) {
+                    count += 1;
+                }
+            }
+            if (count === strs.length) {
+                result += strs[0][i];
+            } else {
+                return result;
+            }
+        }
+
+        return result;
+    }
+}
