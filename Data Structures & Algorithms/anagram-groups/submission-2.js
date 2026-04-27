a9cc9e16ada3faa2bf53c1e01919cee94f1ac50d
Rizqullah T <eryzero@gmail.com>
2026-04-27 16:39:11 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let anagramMap = new Map();
+
+        for (let i = 0; i < strs.length; ++i) {
+            let a = strs[i].split("").sort().join("");
+            anagramMap.set(a, !!anagramMap.get(a) ? anagramMap.get(a).concat([strs[i]]) : [strs[i]]);
+        }
+
+        return Array.from(anagramMap.values());
+    }
+}
